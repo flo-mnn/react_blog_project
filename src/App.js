b6398b529be64b9ea809325922a4088953192348
Florence <monnaie.florence@gmail.com>
2021-01-21 17:22:03 +0100

    banner & cat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import './App.css';
+import React, {Component} from 'react';
+import Home from "./components/Florence";
+import venezia from './components/Home/venice.jpg';
+import istanbul from './components/Home/istanbul.jpg';
+import sydney from './components/Home/sydney.jpg';
+
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 // import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 {/* <FontAwesomeIcon icon={faFacebook} /> */}
 
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  
+  state = {
+    articles : [
+      {titre: `Venezia, la belle au bord de l'eau`, coverImg: venezia, auteur: "Letizia Santina", tags: ['City-Trip','Rencontres']},
+      {titre: `Sydney, au coeur de l'Océanie lointaine`, coverImg: sydney, auteur: "Jack Matthew", tags: ['Off-Road','Foodies']},
+      {titre: `Istanbul, ô cité emplie d'Histoire`, coverImg: istanbul, auteur: "Mehtap M.",tags: ['City-Trip','Foodies','Rencontres']},
+    ],
+    tags : ['Off Road','City Trip','Foodies','Rencontres'],
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="App w-100">
+        <Home leState={this.state}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/florence.js
+++ src/components/florence.js
@@ -1,3 +1,15 @@
-// Home page
-// + navbar
-// + footer
+import React, {Fragment} from 'react'
+import Banner from "./Home/Banner";
+import Categories from './Home/Categories'
+
+const Home = props => {
+    // console.log(props.allArticles[0].coverImg);
+    return(
+        <div id="home">
+           <Banner allArticles={props.leState.articles}/>
+           <Categories tags={props.leState.tags}/>
+        </div>
+    )
+};
+
+export default Home
