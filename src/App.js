7dd779743cbdc042029e125fa2b2c1d23847af52
Florence <monnaie.florence@gmail.com>
2021-01-22 08:37:35 +0100

    Fr 9am

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/Florence";
 import venezia from './components/Home/venice.jpg';
 import istanbul from './components/Home/istanbul.jpg';
 import sydney from './components/Home/sydney.jpg';
+import piter from './components/Home/piter.jpg';
+import newyork from './components/Home/newyork.jpg';
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -17,6 +19,8 @@ class App extends Component{
       {titre: `Venezia, la belle au bord de l'eau`, coverImg: venezia, auteur: "Letizia Santina", tags: ['City-Trip','Rencontres']},
       {titre: `Sydney, au coeur de l'Océanie lointaine`, coverImg: sydney, auteur: "Jack Matthew", tags: ['Off-Road','Foodies']},
       {titre: `Istanbul, ô cité emplie d'Histoire`, coverImg: istanbul, auteur: "Mehtap M.",tags: ['City-Trip','Foodies','Rencontres']},
+      {titre: `Saint-Pétersbourg, la détermination d'un Empire`, coverImg: piter, auteur: "Nadezhda Nikolaeva",tags: ['City-Trip','Foodies']},
+      {titre: `New-York, le rêve américain XXL`, coverImg: newyork, auteur: "Brooke Loyd",tags: ['City-Trip']},
     ],
     tags : ['Off Road','City Trip','Foodies','Rencontres'],
   }
